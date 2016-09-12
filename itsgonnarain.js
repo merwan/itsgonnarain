f11d3226d4d2ae1605605306f79400a569187d18
Merouane Atig <merouane@teezily.com>
2016-09-12 18:36:19 +0200

    Add phase shifting and panning

--- itsgonnarain.js
+++ itsgonnarain.js
@@ -1,17 +1,29 @@
 let audioContext = new AudioContext();
 
+function startLoop(audioBuffer, pan = 0, rate = 1) {
+  let sourceNode = audioContext.createBufferSource();
+  let pannerNode = audioContext.createStereoPanner();
+  let loopStart = 3.5;
+  let loopEnd = 4.3;
+
+  sourceNode.buffer = audioBuffer;
+  sourceNode.loop = true;
+  sourceNode.loopStart = loopStart;
+  sourceNode.loopEnd = loopEnd;
+  sourceNode.playbackRate.value = rate;
+  pannerNode.pan.value = pan;
+
+  sourceNode.connect(pannerNode);
+  pannerNode.connect(audioContext.destination);
+
+  sourceNode.start(0, loopStart);
+}
+
 fetch('itsgonnarain.mp3')
   .then(response => response.arrayBuffer())
   .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
   .then(audioBuffer => {
-    let sourceNode = audioContext.createBufferSource();
-    let loopStart = 3.5;
-    let loopEnd = 4.3;
-    sourceNode.buffer = audioBuffer;
-    sourceNode.loop = true;
-    sourceNode.loopStart = loopStart;
-    sourceNode.loopEnd = loopEnd;
-    sourceNode.connect(audioContext.destination);
-    sourceNode.start(0, loopStart);
+    startLoop(audioBuffer, -1);
+    startLoop(audioBuffer, 1, 1.005);
   })
   .catch(e => console.error(e));
